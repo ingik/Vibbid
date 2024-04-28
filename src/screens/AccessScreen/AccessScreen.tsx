9555c8664e4cfb99cd3460bcb946861f1472fc26
ingik <skskoe@naver.com>
2024-04-28 17:54:03 +0900

    fix: filpper delete, ios scheme fix

--- src/screens/AccessScreen/AccessScreen.tsx
+++ src/screens/AccessScreen/AccessScreen.tsx
@@ -1,15 +1,18 @@
-import { Button, StyleSheet, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
 import { useCustomNavigation } from '../../navigations/hooks';
 import { FunctionComponent } from 'react';
+import Config from 'react-native-config';
 
 const AccessScreen: FunctionComponent = function AccessScreen() {
   const navigation = useCustomNavigation();
+
   const onClick = () => {
     navigation.navigate('Main', { propText: '안녕하세요' });
   };
 
   return (
     <View style={styles.container}>
+      <Text>{Config.ENV}</Text>
       <Button title="button" onPress={onClick} />
     </View>
   );
